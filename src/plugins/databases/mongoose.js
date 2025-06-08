f5b52f6a69f74a49466dbb8dbdb941bef076006d
akkassingh <akkassingh@gmail.com>
2025-06-08 15:45:14 -0700

    inital commit

---
+++ src/plugins/databases/mongoose.js
@@ -0,0 +1,31 @@
+import fp from "fastify-plugin";
+import mongoose from "mongoose";
+
+async function mongoosePlugin(fastify, config) {
+  let mongoStatus = "disconnected";
+
+  // Makes the current connection status available to other plugins
+  // This is used to show the connection status on the home page
+  fastify.decorate("mongoStatus", () => mongoStatus);
+
+  // TODO: Connect to MongoDB
+  try {
+    await mongoose.connect(config.uri, config.otions);
+    mongoStatus = "connected";
+    fastify.log.info("Connected to MongoDB");
+  } catch (err) {
+    fastify.log.error("Error connecting to MongoDB");
+    throw err;
+  }
+
+  // Graceful shutdown
+  fastify.addHook("onClose", async (fastifyInstance, done) => {
+    mongoStatus = "disconnected";
+    // TODO: Close MongoDB connection
+    await mongoose.connection.close();
+    fastify.log.info("Connection to MongoDB closed");
+    done();
+  });
+}
+
+export default fp(mongoosePlugin, { name: "mongoose-plugin" });
