48c6854f993f2a4eb194bfe9d47e05c20dcf2b16
akkassingh <akkassingh@gmail.com>
2025-06-10 00:05:50 -0700

    model and sequel connection

--- src/config/index.js
+++ src/config/index.js
@@ -1,17 +1,23 @@
 export const config = {
   server: {
-    port: process.env.PORT || 3000
+    port: process.env.PORT || 3000,
   },
   mongodb: {
-    uri: "mongodb://localhost:27017/shop",
+    uri: "mongodb://localhost:27017/simpleshop",
     options: {
       serverSelectionTimeoutMS: 3000,
-      socketTimeoutMS: 3000
-    }
+      socketTimeoutMS: 3000,
+    },
+  },
+  mysql: {
+    uri: "mysql://root:secret@localhost:3306/shop",
+    options: {
+      logging: false,
+    },
   },
   session: {
     // Secret key to encrypt client side sessions.
     // Created on the terminal with `node -e "console.log(require('crypto').randomBytes(32).toString('base64'))"`
-    secret: "x3cIkEhWRLRLBD8Zfhd2SUw0UEGieSjOVV2a1a82YEE="
-  }
+    secret: "x3cIkEhWRLRLBD8Zfhd2SUw0UEGieSjOVV2a1a82YEE=",
+  },
 };
